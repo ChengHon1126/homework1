38c0d94cb7474806ae898c33d32b9393eca92113
cheng <lkmd555@gmail.com>
2022-12-08 20:06:36 +0800

    add while

--- src/com/train/Tester.java
+++ src/com/train/Tester.java
@@ -5,6 +5,27 @@ import java.util.Scanner;
 public class Tester {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int buy = 1;
+        while (buy != 0){
+            buy = scanner.nextInt();
+            if (buy != -1){
+            System.out.print("please enter number of ticket:");
+            int tickets = scanner.nextInt();
+            System.out.print("how many round_trip ticket:");
+            int round_trip = scanner.nextInt();
+
+            Test test = new Test(tickets,round_trip);
+            test.print();}
+            else {
+                return;
+            }
+        }
+
+    }
+
+
+    public void testbeta(){
+        Scanner scanner = new Scanner(System.in);
         System.out.print("please enter number of ticket:");
         int tickets = scanner.nextInt();
         System.out.print("how many round_trip ticket:");
--- src/com/trainkotlin/TesterKotlin.kt
+++ src/com/trainkotlin/TesterKotlin.kt
@@ -4,22 +4,28 @@ import java.util.Scanner
 
 fun main() {
     var scanner =Scanner(System.`in`)
-    print("please enter total ticket:");
-    var ticket =scanner.nextInt()
-    print("how many round_trip:")
-    var round = scanner.nextInt()
-    val design= design(ticket,round)
-    design.print()
-    
+    var buy = 1
 
-
+    while (buy != 0){
+            buy = scanner.nextInt()
+        if (buy != -1) {
+            print("please enter total ticket:");
+            var ticket =scanner.nextInt()
+            print("how many round_trip:")
+            var round = scanner.nextInt()
+            val design = design(ticket, round)
+            design.print()
+        }else{
+            return
+        }
+    }
 }
 
 class design(var ticket:Int,var round:Int){
 
     fun print(){
-        print("total ticket:$ticket"+"\n"+"round_trip:$round"+"\n"
-                +"total:"+((ticket-round)*1000+round*2000*0.9).toInt())
+        print("total ticket:$ticket\nround_trip:$round\ntotal:" +
+                "${((ticket-round)*1000+round*2000*0.9).toInt()}")
     }
 
 
